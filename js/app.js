356c7eb2ce72d15762daa4e32cd6de3aa072cc28
altonelli <altonelli14@gmail.com>
2016-03-19 13:39:14 -0700

    Fully functioning Game with score board.

--- js/app.js
+++ js/app.js
@@ -3,26 +3,58 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
-
+  var isWinner = false;
   var player = true;
-  $('.box').click(function gamePlay(){
+  var player1Wins = 0;
+  var player2Wins = 0;
+
+  $('.box').click(function(){
     if(!$(this).text()){
       if(player === true){
         $(this).text("X");
       } else {
         $(this).text("O");
       }
-      player = !player;
+      //player = !player;
     } else {
       alert("That spot has been taken, try again!");
     }
 
-    winner2(winCheck,$('.box'),$(this).text());
+    var winner = winner2(winCheck,isWinner,$(this).text());
+    if(winner === "X"){
+      console.log($('#player1').val());
+      isWinner = true;
+      player = true;
+      player1Wins++;
+      $('#player1').text(player1Wins);
+    } else if (winner === "O") {
+      console.log('in o winner');
+      isWinner = true;
+      player = false;
+      var newVal = parseInt($('#player2').val()) + 1;
+      player2Wins++;
+      $('#player2').text(player2Wins);
+    }
+
+    console.log(isWinner);
+
+    if(isWinner){
+      var playAgain = prompt("Would you like to play again? 'yes' or 'no'");
+      if(playAgain === "yes"){
+        isWinner = false;
+        $('.box').text('');
+        $('.box').css("background-color", "white");
+      }
+    } else {
+      player = !player;
+    }
+
+
   });
 
 });
 
-function winner2(hash, arr, letter){
+function winner2(hash, isWinner, letter){
   for(var key in hash){
     //console.log('win2: ' + $(this).attr("data-position") + " " + $('[data-position="' + hash[key][0] + '"]').text());
     if(
@@ -31,6 +63,9 @@ function winner2(hash, arr, letter){
       ($('[data-position="' + hash[key][1] +'"]').text() === letter)){
         $(key).css("background-color", "green");
         alert(letter + " won with " + key);
+        isWinner = true;
+        console.log("isWinner from winner2 " + isWinner);
+        return letter;
       }
   }
 }
